ace1c3f60adf484a5b98cf3d2768a0a54a3727a6
Theodor Östling <theos327@student.liu.se>
2023-11-13 10:16:04 +0100

    Fixade lite på normaliseringen

--- Theo/faceNormalization.m
+++ Theo/faceNormalization.m
@@ -1,4 +1,4 @@
-function faceNormalization(img, eye1, eye2)
+function normalized_img = faceNormalization(img, eye1, eye2, targetSize)
     % Calculate the angle between the eyes
     angle = atan2(eye2(2) - eye1(2), eye2(1) - eye1(1));
 
@@ -8,10 +8,24 @@ function faceNormalization(img, eye1, eye2)
     % Rotate the image to make the eyes horizontal
     rotated_img = imrotate(img, -angle_degrees, 'bilinear', 'crop');
 
-    % Display the original and rotated images for comparison
-    figure;
-    subplot(1, 2, 1), imshow(img), title('Original Image');
-    subplot(1, 2, 2), imshow(rotated_img), title('Rotated Image');
+    % Calculate the center of the eyes in the rotated image
+    eye_center = [(eye1(1) + eye2(1)) / 2, (eye1(2) + eye2(2)) / 2];
 
-    % Now you can proceed with further normalization steps
+    % Calculate the translation needed to center the eyes
+    translation = round(size(rotated_img) / 2 - eye_center);
+
+    % Translate the rotated image
+    translated_img = imtranslate(rotated_img, translation, 'bilinear', 'FillValues', 0);
+
+    % Resize the translated image to the target size
+    normalized_img = imresize(translated_img, targetSize);
+
+    % Display the original, rotated, translated, and normalized images for comparison
+    figure;
+    subplot(1, 4, 1), imshow(img), title('Original Image');
+    subplot(1, 4, 2), imshow(rotated_img), title('Rotated Image');
+    subplot(1, 4, 3), imshow(translated_img), title('Translated Image');
+    subplot(1, 4, 4), imshow(normalized_img), title('Normalized Image');
+
+    % Now you can proceed with any additional processing steps
 end
