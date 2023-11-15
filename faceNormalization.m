b56fc9f11fe97c67b2b33a114d5108bb53f12315
Grantallkotten <143391787+Grantallkotten@users.noreply.github.com>
2023-11-15 13:39:09 +0100

    FaceNormolazation done

--- faceNormalization.m
+++ faceNormalization.m
@@ -1,4 +1,20 @@
-function normalized_img = faceNormalization(img, eye1, eye2)
+function imgNormalized = faceNormalization(img, eye1, eye2)
+    %% Translation
+    % Specify the desired center coordinates
+    desiredCenter =  [(eye1(1) + eye2(1)) / 2, (eye1(2) + eye2(2)) / 2];
+    
+    % Calculate the displacement from the current center to the desired center
+    currentCenter = [size(img, 2)/2, size(img, 1)/2]; % Current center of the image
+    translationVector = round(desiredCenter - currentCenter);
+    
+    % Translate the image
+    imgTranslated = imtranslate(img, translationVector);
+    
+    % Translate the point coordinates
+    eye1 = eye1 + translationVector;
+    eye2 = eye2 + translationVector;
+
+    %% Rotation
     % Calculate the angle between the eyes
     if eye1(1) > eye2(1)
         dummy = eye2;
@@ -8,35 +24,45 @@ function normalized_img = faceNormalization(img, eye1, eye2)
     angle = (atan2(eye2(2) - eye1(2), eye2(1) - eye1(1)));
 
     % Rotate the image to make the eyes horizontal
-    rotated_img = imrotate(img, rad2deg(angle), 'bicubic', 'crop');
+    rotated_img = imrotate(imgTranslated, rad2deg(angle), 'bicubic', 'crop');
     
+    %% Scale
     % Calculate the center of the eyes in the rotated image
+    currentDistance = sqrt((eye2(1) - eye1(1))^2 + (eye2(2) - eye1(2))^2);
+    
+    % Specify the desired distance between the two pixels after scaling
+    desiredDistance = 120;
+    
+    % Calculate the scale factor
+    scaleFactor = desiredDistance / currentDistance;
+    
+    % Scale the image
+    scaledImage = imresize(rotated_img, scaleFactor);
+    
+    % Scale the pixel coordinates
+    eye1 = round(eye1 * scaleFactor);
+    eye2 = round(eye2 * scaleFactor);
+
+    %% Crop
     eye_center = [(eye1(1) + eye2(1)) / 2, (eye1(2) + eye2(2)) / 2];
 
-
-    %% Gör så distance_between_eyes är densamma för alla bilder, dvs skala om bilden
-
-    % Calculate the distance between the eyes
-    distance_between_eyes = norm(eye2 - eye1);
-
     % Calculate the cropping window
-    crop_width = 50 + distance_between_eyes / 2;
-    crop_height_above = 100;
-    crop_height_below = 150;
+    cropWidth = 50 + 120 / 2;
+    cropHeightAbove = 100;
+    cropHeightBelow = 150;
 
-    crop_x = round([eye_center(1) - crop_width, eye_center(1) + crop_width]);
-    crop_y = round([eye_center(2) - crop_height_above, eye_center(2) + crop_height_below]);
+    cropX = round([eye_center(1) - cropWidth, eye_center(1) + cropWidth]);
+    cropY = round([eye_center(2) - cropHeightAbove, eye_center(2) + cropHeightBelow]);
 
     % Ensure the cropping window stays within the image boundaries
-    crop_x = max(1, crop_x);
-    crop_x = min(size(rotated_img, 2), crop_x);
-    crop_y = max(1, crop_y);
-    crop_y = min(size(rotated_img, 1), crop_y);
+    cropX = max(1, cropX);
+    cropX = min(size(scaledImage, 2), cropX);
+    cropY = max(1, cropY);
+    cropY = min(size(scaledImage, 1), cropY);
 
     % Crop the image with the fixed window
-    cropped_img = rotated_img(crop_y(1):crop_y(2), crop_x(1):crop_x(2), :);
-
+    imgCropped = scaledImage(cropY(1):cropY(2), cropX(1):cropX(2), :);
 
     % Return the normalized image without resizing
-    normalized_img = cropped_img;
+    imgNormalized = imgCropped;
 end
